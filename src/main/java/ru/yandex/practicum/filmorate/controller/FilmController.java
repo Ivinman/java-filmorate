e427dc1c2c8b93bc855f2045a47b94840688c1dd
Aleksey <aleksiei.ivin.97@yandex.ru>
2024-03-17 20:55:51 +0300

    feat: первый коммит приложения

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,89 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.AlreadyExistException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int id = 0;
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) throws Exception {
+        if (!validation(film)) {
+            log.info("Ошибка валидации");
+            throw new ValidationException("Ошибка валидации");
+        }else if (films.containsValue(film)) {
+            log.info("Добавление через POST запрос уже имеющегося объекта");
+            throw new AlreadyExistException("Данный фильм уже добавлен");
+        } else {
+            id++;
+            film.setId(id);
+            films.put(id, film);
+            log.info("Новый фильм добавлен в общий список");
+            return film;
+        }
+    }
+
+    @PutMapping
+    public Film addOrUpdateFilm (@RequestBody Film film) throws Exception {
+        if (!validation(film)) {
+            log.info("Ошибка валидации");
+            throw new ValidationException("Ошибка валидации");
+        }else {
+            if (film.getId() == 0) {
+                id++;
+                film.setId(id);
+                log.info("Новый фильм добавлен в общий список");
+                films.put(id, film);
+                return film;
+            }else {
+                if (films.containsKey(film.getId())) {
+                    log.info("Был обновлён фильм под названием: {}", film.getName());
+                    films.put(id, film);
+                    return film;
+                }else {
+                    throw new ValidationException("Попытка обновления предварительно не добавленного объекта");
+                }
+                //film.setId(films.get(film.getName()).getId());
+                //log.info("Был обновлён фильм под названием: {}", film.getName());
+            }
+            //films.put(id, film);
+            //return film;
+        }
+    }
+
+    @GetMapping
+    public List<Film> getAll() {
+        List<Film> filmsList = new ArrayList<>();
+        for (int i=1; i<= films.size(); i++) {
+            for (Film film : films.values()) {
+                if (film.getId() == i) {
+                    filmsList.add(film);
+                }
+            }
+        }
+        return filmsList;
+        //return new ArrayList<>(films.values());
+    }
+
+    private boolean validation(Film film) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return film.getName() != null
+                && !film.getName().isEmpty()
+                && !film.getName().isBlank()
+                && film.getDescription().length() <= 200
+                && LocalDate.parse(film.getReleaseDate(), formatter)
+                    .isAfter(LocalDate.parse("1895-12-28", formatter))  //film.getReleaseDate().isAfter(LocalDate.parse("28.12.1895", DateTimeFormatter.ofPattern("dd.MM.yyyy")))
+                && film.getDuration() > 0;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,96 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.AlreadyExistException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int id = 0;
+
+    @PostMapping
+    public User addUser (@RequestBody User user) throws Exception {
+        if (!validation(user)) {
+            log.info("Ошибка валидации");
+            throw new ValidationException("Ошибка валидации");
+        }else if (users.containsValue(user)) {
+            log.info("Добавление через POST-запрос уже имеющегося объекта");
+            throw new AlreadyExistException("Данный пользователь уже добавлен");
+        }else {
+            if(user.getName() == null || user.getName().isEmpty() || user.getName().isBlank()) {
+                user.setName(user.getLogin());
+            }
+            id++;
+            user.setId(id);
+            users.put(id, user);
+            log.info("Новый пользователь добавлен в общий список");
+            return user;
+        }
+    }
+
+    @PutMapping
+    public User addOrUpdateUser (@RequestBody User user) throws Exception{
+        if (!validation(user)) {
+            log.info("Ошибка валидации");
+            throw new ValidationException("Ошибка валидации");
+        }else {
+            if(user.getName().isEmpty() || user.getName().isBlank()) {
+                user.setName(user.getLogin());
+            }
+            if (user.getId() == 0) {
+                id++;
+                user.setId(id);
+                log.info("Новый пользователь добавлен в общий список");
+                users.put(user.getId(), user);
+                return user;
+            }else {
+                if (users.containsKey(user.getId())) {
+                    log.info("Был обновлён пользователь с почтой: {}", user.getEmail());
+                    users.put(user.getId(), user);
+                    return user;
+                }else {
+                    throw new ValidationException("Попытка обновления предварительно не добавленного объекта");
+                }
+                //user.setId(users.get(user.getEmail()).getId());
+                //log.info("Был обновлён пользователь с почтой: {}", user.getEmail());
+            }
+            //users.put(user.getId(), user);
+            //return user;
+        }
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        List<User> usersList = new ArrayList<>();
+        for (int i=1; i<= users.size(); i++) {
+            for (User user : users.values()) {
+                if (user.getId() == i) {
+                    usersList.add(user);
+                }
+            }
+        }
+        return usersList;
+        //return new ArrayList<>(users.values());
+    }
+
+    private boolean validation(User user) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return user.getEmail() != null
+                &&!user.getEmail().isBlank()
+                && !user.getEmail().isEmpty()
+                && user.getEmail().contains("@")
+                && !user.getLogin().isBlank()
+                && !user.getLogin().isEmpty()
+                && !user.getLogin().contains(" ")
+                && !LocalDate.parse(user.getBirthday(), formatter).isAfter(LocalDate.now());
+    }
+}
